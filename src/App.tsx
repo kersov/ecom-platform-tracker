094ba00665d923061cd377cac19bc1cb521e4f91
Oleksii Kersov <kersov@gmail.com>
2025-11-09 02:07:05 +0200

    Fixed Github pages issue

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        basename={import.meta.env.PROD ? "/ecom-platform-tracker" : "/"}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
